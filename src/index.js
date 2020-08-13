c3499690c15197cf9ddb64d4d48cdf1025af6e3d
Vadim <40899603+vvbauer@users.noreply.github.com>
2020-08-13 20:04:48 +0300

    home work 1st day

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
+import App from './App';
+import './normalize.css';
+import './style.css';
 
-const Header = ({text}) => <h1 id='hi'>{text}</h1>;
+// const Header = ({text}) => <h1 id='hi'>{text}</h1>;
 
-const Element = <p>Paragraph</p>;
+// const Element = <p>Paragraph</p>;
 
-class Main extends React.Component {
-  render() {
-    return (
-      <div className="main">
-        <Header text={'property text'}/>
-        {Element}
-      </div>
-    )
-  }
-}
+// class Main extends React.Component {
+//   render() {
+//     return (
+//       <div className="main">
+//         <Header text={'property text'}/>
+//         {Element}
+//       </div>
+//     )
+//   }
+// }
 
 ReactDOM.render(
   <React.StrictMode>
-    <Main />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
